f835c97fc5f1058e0522a2f3153f287ef2db14ca
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-11 10:40:36 +0530

    [Raj] Added:- The test case 1.1 for sad message

---
+++ TestingMoodAnalyser/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingMoodAnalyser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingMoodAnalyser")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f4b1613-d313-44b4-81f0-918c069c4601")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingMoodAnalyser/TestingMood.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+using System;
+
+namespace TestingMoodAnalyser
+{
+    [TestClass]
+    public class TestingMood
+    {
+        [TestCategory("Sad Message")]
+        [TestMethod]
+        public void TestSadMoodMessage()
+        {
+            ///AAA
+            ///Arange
+            string msg = "I am in Sad Mood";
+            string expected = "sad";
+            MoodAnalyse mood = new MoodAnalyse();
+
+            ///Act
+            string actual = mood.AnalyzeMood(msg);
+          
+            ///Asert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
