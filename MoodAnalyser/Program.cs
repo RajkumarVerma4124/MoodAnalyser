117918d033c5e2f00f25981d407bda01cc34b601
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-11 10:14:46 +0530

    [Raj] Added:- The welcome message in main program

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    /// <summary>
+    /// Mood analyser program to analyse the mood
+    /// </summary>
+    public class Program
+    {
+        //Entry point to the program
+        public static void Main(string[] args)
+        {
+            //Displaying the welcome message
+            Console.WriteLine("Welcome To The Mood Analyser Program");
+            Console.ReadLine();
+        }
+    }
+}
